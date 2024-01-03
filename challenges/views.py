9cfe4733db92bcca22466649f40e8c9804ccfedf
muchiri <paul.muchiri@copiakenya.com>
2024-01-04 00:46:00 +0300

    added dynamic fuunction

--- challenges/views.py
+++ challenges/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
 
-def january(request):
-    return HttpResponse("eat no meat for the entire month")
+def monthly_challenge(request, month):
+    challeng_text = None
+    if month == "january":
+        challeng_text = "eat no meat for the entire month"
 
+    elif month == "february":
+        challeng_text = "Walk for at least 20 min every day"
 
-def february(request):
-    return HttpResponse("Walk for at least 20 min every day")
+    elif month == "march":
+        challeng_text = "Learn Django for 20 min every day"
+
+    else:
+        return HttpResponseNotFound("This month is invalid!")
+
+    return HttpResponse(challeng_text)
